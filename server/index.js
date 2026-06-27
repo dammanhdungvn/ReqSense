c3a24bd1944714a1cb4510d147023103d3af3748
Đỗ Kiên <kien.dt224869@sis.hust.edu.vn>
2026-06-27 15:09:15 +0700

    Configure Railway deployment

--- server/index.js
+++ server/index.js
@@ -17,6 +17,21 @@ app.use('/api', chatRoute);
 app.use('/api', evaluateRoute);
 app.use('/api', uploadRoute);
 
+const clientDist = path.resolve(__dirname, '..', 'client', 'dist');
+
+app.get('/health', (_req, res) => {
+  res.json({ status: 'ok' });
+});
+
+app.use(express.static(clientDist));
+
+app.get('*', (req, res, next) => {
+  if (req.path.startsWith('/api')) return next();
+  return res.sendFile(path.join(clientDist, 'index.html'), err => {
+    if (err) next();
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`ReqSense server running on http://localhost:${PORT}`);
 });
